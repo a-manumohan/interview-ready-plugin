0506a80b1ccb4e364585829d5ec09c9eeeb867df
Manu Mohan <a.manumohan@gmail.com>
2026-03-19 15:11:36 -0400

    Fix KSP plugin version to use new versioning scheme (2.3.6)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     // These are needed so the convention plugin can apply AGP + Kotlin + KSP plugins
     implementation("com.android.tools.build:gradle:8.8.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.20")
-    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.3.20-1.0.31")
+    implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.3.6")
 }
 
 gradlePlugin {
